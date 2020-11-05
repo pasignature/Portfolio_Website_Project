5a6edc6047f3de1d0ddbf77b3145df3211acf628
pasignature <pasignature@gmail.com>
2020-11-05 15:08:11 +0100

    first commit

---
+++ action_scripts/pdoconnection.php
@@ -0,0 +1,31 @@
+<?php
+//For security reasons, don't display any errors or warnings. Comment out in DEV.
+error_reporting(0);
+
+//Africa/Lagos default timezone for all datetime
+date_default_timezone_set('Africa/Lagos');
+
+//connect to database
+function dbconnect(){
+	//toggle database setup - production mode/development mode
+	$development = true;
+
+	$servername = "localhost"; $username = "dffsdf7f878f"; $password = "455555uuuuuu"; $dbname = "websitedb";
+
+	if($development){
+	$servername = "localhost"; $username = "root"; $password = ""; $dbname = "myportfolio_websitedb";
+	}
+
+	try{
+		$pdoconn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		$pdoconn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//Connected to Database;
+		return $pdoconn;
+	}catch(PDOException $pdoerror){
+		return "OOPs Connection Failed.".$pdoerror;
+	}
+
+return "OOPs something went wrong.";
+}
+
+?>
